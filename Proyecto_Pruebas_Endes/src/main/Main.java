dac114699973abdc246634cdb624926bf061c70f
isablpf <mijr@educastur.org>
2020-11-27 11:11:01 +0100

    Initial Commit

---
+++ Proyecto_Pruebas_Endes/src/main/Main.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package main;
+
+import java.util.Scanner;
+
+
+
+/**
+ * 
+ * @author: Isabel Jardón
+ * 
+ * Fecha: 23 oct. 2020
+ *
+ * @version: 1.0
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Preparo para poder leer de teclado
+		Scanner teclado = new Scanner(System.in);
+
+		int numero1, numero2;
+		System.out.println("Introduzca un valor para el número 1: ");
+		numero1 = teclado.nextInt();
+		System.out.println("Introduzca un valor para el númeor 2: ");
+		numero2 = teclado.nextInt();
+		
+		System.out.println(numero1+ "-"+numero2+"="+(numero1-numero2));
+		
+		teclado.close();
+
+	}
+
+	
+	
+}
---
+++ Proyecto_Pruebas_Endes/src/modelo/Persona.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * Clase Persona
+ * 
+ * Representa a un objeto Person
+ * 
+ * @author: Isabel Jardón
+ *  
+ * Fecha: 23 oct. 2020
+ *
+ * @version: 
+ *
+ */
+public class Persona {
+
+		
+}
