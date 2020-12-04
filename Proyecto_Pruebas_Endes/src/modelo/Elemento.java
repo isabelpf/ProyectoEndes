59204d872ed1e8a7668ea8d16e12d00abfd09b2a
isablpf <mijr@educastur.org>
2020-12-04 19:50:33 +0100

    Commint New Class Elemento

---
+++ Proyecto_Pruebas_Endes/src/modelo/Elemento.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+* @author: Isabel Jardón
+* Fecha: 4 dic. 2020
+* version: 
+*
+**/
+public class Elemento {
+	private int numero;
+	private String descripcion;
+	public Elemento(int numero, String descripcion) {
+		super();
+		this.numero = numero;
+		this.descripcion = descripcion;
+	}
+	
+	
+}
